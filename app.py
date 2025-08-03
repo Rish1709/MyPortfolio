19b00b5c4aab23ba796b534fe2693552ef517bf7
Rish1709 <146486547+Rish1709@users.noreply.github.com>
2025-08-03 23:40:17 +0530

    Update app.py
    changes app.run() for deployment

--- app.py
+++ app.py
@@ -139,4 +139,5 @@ if __name__ == "__main__":
             db.session.add(me)
             db.session.commit()
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
